9419f6bc287e6c3447b72e4aed3b8d43c4d47c68
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-09-07 14:26:30 +0400

    feat: add home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,81 @@
-import Head from "next/head";
+import { Layout } from "../components/Layout/Layout";
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+import { GetStaticPropsContext } from "next";
+import Image from "next/image";
 
-      <title>Title</title>
+import bed from "../public/content/bed/classic/main.jpg";
+import carpet from "../public/content/carpet/classic/main.jpg";
 
-      <meta name="description" content="Description" />
-      <meta property="og:title" content="Title" />
-      <meta property="og:description" content="Description" />
-      <meta property="og:image" content="https://www.mywebsite.com/image.jpg" />
-      <meta property="og:image:alt" content="Image alt" />
-      <meta property="og:locale" content="ru_RU" />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="og:url" content="" />
-
-      <link rel="icon" href="/favicon.ico" />
-      {/* <!-- 32×32 -->*/}
-      <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-      {/*<!-- 512×512 -->*/}
-      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      {/*<!-- 180×180 -->*/}
-      <link rel="manifest" href="/manifest.webmanifest" />
-      <meta name="theme-color" content="#FF00FF" />
-    </Head>
-    <main></main>
-  </div>
-);
+const Home = ({ page }) => {
+  return (
+    <Layout>
+      <div className="relative bg-white overflow-hidden">
+        <div className="py-12 sm:py-24">
+          <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:static sm:flex lg:flex-col-reverse">
+            <div className="mt-2 sm:mr-10 sm:mt-2 lg:mt-10 ">
+              <ul className="flex sm:flex-col lg:flex-row gap-10 mb-10">
+                <li className="">
+                  <a href="">
+                    <Image
+                      className="rounded-md max-w-7xl"
+                      src={bed}
+                      alt="Picture of the author"
+                      width={320}
+                      height={320}
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    <Image
+                      className="rounded-md max-w-7xl"
+                      src={carpet}
+                      alt="Picture of the author"
+                      width={320}
+                      height={320}
+                    />
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="sm:max-w-lg">
+              <h1 className="text-4xl font font-extrabold tracking-tight text-gray-900 sm:text-6xl">{page.title}</h1>
+              <p className="mt-4 text-xl text-gray-500">{page.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default Home;
+
+export function getStaticProps({ locale }: GetStaticPropsContext) {
+  const pages = {
+    en: {
+      title: "Sadhu",
+      description: "desc",
+    },
+    ru: {
+      title: "Уникальные доска и ковер для йоги, которые прослужат вам всю жизнь.",
+      description:
+        "Уникальные доски и ковер для поверхности спины, крестца и шеи. Терапевтическая садху-доска с гвоздями —" +
+        " идеальный" +
+        " инструмент для глубокого расслабления, при болях в плечах и пояснице, интенсивных физических нагрузках, а" +
+        " также в качестве духовной практики.",
+    },
+  };
+
+  return {
+    props: {
+      // You can get the messages from anywhere you like, but the recommended
+      // pattern is to put them in JSON files separated by language and read
+      // the desired one based on the `locale` received from Next.js.
+      messages: {
+        ...require(`../messages/shared/${locale}.json`),
+      },
+      page: pages[locale],
+    },
+  };
+}
