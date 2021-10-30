6c03a3214f776070a51f18973a200946c7c44e09
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-10-31 02:50:39 +0400

    fix: local storage key

--- hooks/useReferralCode.ts
+++ hooks/useReferralCode.ts
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { useEffect } from "react";
 
-const REFERRAL_CODE_KEY = "referral_code";
+const REFERRAL_CODE_KEY = "flowtradition_referral_code";
 
 export const useReferralCode = (key = REFERRAL_CODE_KEY, initialValue = "") => {
   const { query } = useRouter();
