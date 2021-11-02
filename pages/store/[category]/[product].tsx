a0c0e914d609ca06c6f780ea6be3c940703a92a7
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-11-02 13:06:56 +0400

    chore: remove comments to redeploy dev

--- pages/store/[category]/[product].tsx
+++ pages/store/[category]/[product].tsx
@@ -381,9 +381,6 @@ export async function getStaticProps({ locale, params, preview }: GetStaticProps
   return {
     props: {
       page,
-      // You can get the messages from anywhere you like, but the recommended
-      // pattern is to put them in JSON files separated by language and read
-      // the desired one based on the `locale` received from Next.js.
       messages: {
         ...require(`../../../messages/shared/${locale}.json`),
         ...require(`../../../messages/store/product/${locale}.json`),
